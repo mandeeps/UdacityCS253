cc0a4b5ecf5fc76ec15339f1c025c66eed687827
Mandeep Shergill <mandeep@server.fake>
2014-01-17 17:55:12 -0500

    hw2

--- hello.py
+++ hello.py
@@ -1,10 +1,28 @@
 import webapp2
+import string
+
+form = """
+<body>
+	<form method="post" action="/redirect">
+		<textarea name="text"></textarea>
+		<input type="submit"></input>
+	</form>
+</body>
+"""
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
+		self.response.headers['Content-Type'] = 'text/html'
+		self.response.write(form)
+
+class RedirectHandler(webapp2.RequestHandler):
+	def post(self):
 		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('Hello, Udacity!')
+		plain = self.request.get("text")
+		crypto = plain.encode('rot13')
+		self.response.write(crypto)
 
 app = webapp2.WSGIApplication([
 	('/', MainPage),
+	('/redirect', RedirectHandler)
 ], debug=True)
